3ac0e3de8f4caf0abaa698ea328d1d03130b4242
hs <hesenhesen@corp.netease.com>
2022-04-15 18:58:31 +0800

    update

--- src/constants/collison.ts
+++
@@ -1,8 +0,0 @@
-export const COLLISON = {
-  PLAYER: -1,
-  MONSTER: -2,
-  LAYERS: {
-    PLAYER: 2,
-    MONSTER: 4
-  }
-} as const;
--- src/constants/scenes.ts
+++
@@ -1,6 +0,0 @@
-export const SCENES = {
-  GAME_MANAGER: 'GameManager',
-  HUD: 'Hud',
-  BASE_LEVEL: 'BaseLevel',
-  MAZE_LEVEL: 'MazeLevel',
-} as const;
